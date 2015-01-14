9137a2b3a717528788e9257cc04d6b2a1a07b96f
chloeLu <chloe.lxd@gmail.com>
2015-01-14 23:05:50 +0800

    Moved main fn to MiniProjMain

--- src/proj/MiniProjMain.java
+++ src/proj/MiniProjMain.java
@@ -1,5 +1,15 @@
 package proj;
 
-public class Main {
-
+public class MiniProjMain {
+	public static void main(String[] args) {
+		try {
+			TradeFileReader reader = new TradeFileReader();
+//			reader.preProcessRawTradeFile("E:\\Working\\workspaceLuna\\MiniProj-java\\data\\taqtrade20131218");
+			System.out.println(System.nanoTime());
+			reader.preProcessRawNbboFile( "E:\\Working\\workspaceLuna\\MiniProj-java\\data\\taqnbbo20131218");
+			System.out.println(System.nanoTime());
+		} catch (Exception e) {
+			// do nothing
+		}
+	}
 }
--- src/proj/TradeFileReader.java
+++ src/proj/TradeFileReader.java
@@ -100,16 +100,4 @@ public class TradeFileReader {
 			System.err.println("IOException: " + ioe.getMessage());
 		}
 	}
-
-	public static void main(String[] args) {
-		try {
-			TradeFileReader reader = new TradeFileReader();
-//			reader.preProcessRawTradeFile("E:\\Working\\workspaceLuna\\MiniProj-java\\data\\taqtrade20131218");
-			System.out.println(System.nanoTime());
-			reader.preProcessRawNbboFile( "E:\\Working\\workspaceLuna\\MiniProj-java\\data\\taqnbbo20131218");
-			System.out.println(System.nanoTime());
-		} catch (Exception e) {
-			// do nothing
-		}
-	}
 }
