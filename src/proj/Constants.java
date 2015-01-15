105db43c8d883b3e42fb45dcae29ace4c074593c
chloeLu <chloe.lxd@gmail.com>
2015-01-15 14:23:53 +0800

    + constants

---
+++ src/proj/Constants.java
@@ -0,0 +1,9 @@
+package proj;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class Constants {
+	public static final String outDir = "..\\project";
+	public static Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+}
